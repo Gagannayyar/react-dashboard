ac49fd1ef18033bca087c333b7171e59049e7799
Gagan Nayyar <62588211+Gagannayyar@users.noreply.github.com>
2022-12-16 06:32:17 +0000

    Day 1

--- intro.js
+++ intro.js
@@ -1,3 +1,4 @@
-function intro() {
-    document.write(intro);
-}
+console.log('Welcome to 30DaysOfJavaScript')
+let firstName = "Gagan"
+let learning = "JavaScript"
+console.log(firstName, "is learning ",learning, "which is a type of ", typeof learning)
