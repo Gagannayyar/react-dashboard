3470665d7ee6e9649cc176e823e7701afdb43cca
GaganNayyarHepta <gagan@heptagon.in>
2023-06-24 15:08:00 +0530

    Frontend Initiated

--- intro.js
+++
@@ -1,4 +0,0 @@
-console.log('Welcome to 30DaysOfJavaScript')
-let firstName = "Gagan"
-let learning = "JavaScript"
-console.log(firstName, "is learning ",learning, "which is a type of ", typeof learning)
